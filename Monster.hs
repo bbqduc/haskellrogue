ba7fc1b7049fd97def3fe5d299b1e4d90e4c2242
bbqduc <brunoankka@gmail.com>
2012-03-31 12:47:11 +0300

    added imports 'as'

--- Monster.hs
+++ Monster.hs
@@ -1,11 +1,11 @@
 module Monster where
 
-import Vec2
+import Vec2 as Vec2
 
-data Monster = Monster { pos :: Vec2 }
+data Monster = Monster { pos :: Vec2.Vec2 }
 
 moveMonsters :: [Monster] -> Int -> Int -> [Monster]
 moveMonsters [] _ _ = []
 moveMonsters (m:ms) pX pY = newM:(moveMonsters ms pY pX)
-                              where newPos = approach (pos m) (Vec2 pY pX)
+                              where newPos = Vec2.approach (pos m) (Vec2.Vec2 pY pX)
 			            newM = Monster { pos = newPos }
--- main.hs
+++ main.hs
@@ -1,23 +1,23 @@
 module Main where
 
-import Vec2
-import Monster
+import Vec2 as Vec2
+import Monster as Mon
 import UI.HSCurses.Curses
 import Data.List
 import Control.Monad
 
 castEnum = toEnum . fromEnum
 
-moveAbout :: [Monster] -> Int -> Int -> IO ()
+moveAbout :: [Mon.Monster] -> Int -> Int -> IO ()
 moveAbout ms pY pX = do
 	erase
 
-	forM (map pos ms) $ \(Vec2 x y) -> (mvAddCh x y (castEnum 'x'))
+	forM (map pos ms) $ \(Vec2.Vec2 x y) -> (mvAddCh x y (castEnum 'x'))
 	mvAddCh pY pX (castEnum '@')
 	
 	refresh
 
-	let nms = moveMonsters ms pX pY
+	let nms = Mon.moveMonsters ms pX pY
 	c <- getCh
 	case c of
 		KeyUp -> moveAbout nms (pY - 1) pX
@@ -30,7 +30,7 @@ main = do
 	initCurses
 	keypad stdScr True
 	echo False
-	let monsters = [Monster { pos = Vec2 1 1 }]
+	let monsters = [Mon.Monster { pos = Vec2.Vec2 1 1 }]
 	cursSet CursorInvisible
 	(sizeY, sizeX) <- scrSize
 	moveAbout monsters (sizeY `div` 2) (sizeX `div` 2)
